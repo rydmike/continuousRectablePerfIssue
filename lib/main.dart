c607eaee19fe21c64b07e6ba516b4020d3dd237f
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-06-12 18:04:13 +0300

    Experiment with some options for the perf issue

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+          ),
+          useMaterial3: true),
+      darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.dark,
+            seedColor: Colors.blue,
+          ),
+          useMaterial3: true),
       home: const MyHomePage(title: 'Performance'),
     );
   }
@@ -36,8 +45,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final Color primary = Theme.of(context).colorScheme.primary;
+    final Color onPrimary = Theme.of(context).colorScheme.onPrimary;
+
     return Scaffold(
-        // backgroundColor: context.colorScheme.background,
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           elevation: 0,
@@ -45,20 +56,20 @@ class _MyHomePageState extends State<MyHomePage> {
           automaticallyImplyLeading: false,
           scrolledUnderElevation: 0,
           backgroundColor: Colors.transparent,
+          foregroundColor: onPrimary,
           title: Row(
             children: [
               // const SizedBox(width: 8,),
               ClipPath(
                 clipper: const ShapeBorderClipper(
                   shape: ContinuousRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(24),
-                    ),
-                  ),
+                      borderRadius: BorderRadius.all(Radius.circular(24))),
                 ),
-                child: Container(
-                  color: Colors.blue.withOpacity(0.3),
-                  child: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back_rounded)),
+                child: ColoredBox(
+                  color: primary.withOpacity(0.3),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_back_rounded)),
                 ),
               ),
               const Expanded(
@@ -84,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
               child: Container(
-                color: Colors.blue.withOpacity(0.7),
+                color: primary.withOpacity(0.7),
               ),
             ),
           ),
@@ -107,8 +118,229 @@ class _MyHomePageState extends State<MyHomePage> {
                           const SizedBox(
                             height: 120,
                           ),
-                          Row(
-                            children: const [
+                          const Row(
+                            children: [
+                              Text(
+                                'Temperature',
+                              ),
+                              Expanded(
+                                child: SizedBox(),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'Celsius °C',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'Fahrenheit °F',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'Kelvin K',
+                                circle: circle,
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Speed',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'km/h',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'mph',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'kts',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'm/s',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'Beaufort',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Rain',
+                          ),
+                          const SizedBox(height: 10),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'mm',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'cm',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'in',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Pressure',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'mbar',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'inHg',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'psi',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'mmHg',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'kPa',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Distance',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'km',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'mi',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Time',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: '12h',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: '24h',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Air Quality',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'US - EPA',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'UK Defra',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Air Quality Guidance Sensitivity',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: [
+                              _SingleOption(
+                                text: 'General population',
+                                circle: circle,
+                              ),
+                              _SingleOption(
+                                text: 'At risk population',
+                                circle: circle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Row(
+                            children: [
                               Text(
                                 'Temperature',
                               ),
@@ -350,18 +582,15 @@ class _SingleOption extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return FilledButton(
         onPressed: () {},
-        style: ElevatedButton.styleFrom(
+        style: FilledButton.styleFrom(
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-            backgroundColor: Colors.blue,
             shape: circle
-                ? const ContinuousRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(32)))
-                : RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), // <-- Radius
-                  )),
-        child: Text(
-          text,
-        ));
+                ? const ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32)))
+                : const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)))),
+        child: Text(text));
   }
 }
